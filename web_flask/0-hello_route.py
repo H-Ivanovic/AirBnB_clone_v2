a54b6028e679563e7f7315c2081b53490ac29ada
H-Ivanovic <hamalaivan808@gmail.com>
2023-04-26 05:53:01 -0700

    tas

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,27 @@
-#!/usr/bin/python3
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/env bash
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-@app.route('/')
-def hello():
-    '''
-    Hello Flask route
-    '''
-    return 'Hello HBNB!'
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
+sudo chown -R ubuntu:ubuntu /data/
 
-if __name__ == '__main__':
-    app.run()
-    app.url_map.strict_slashes = False
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
